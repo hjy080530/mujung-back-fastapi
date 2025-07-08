297e0a6a07d48790455c59846a975a01466c7a17
hjy080530 <bomaye119@gmail.com>
2025-07-08 11:00:10 +0900

    chore :: votes 타입 맞추기

--- app/services/votes.py
+++ app/services/votes.py
@@ -1,5 +1,9 @@
-from app.services.supabase import supabase
+from pydantic import BaseModel
 
+from app.services.supabase import supabase
+class VoteCreate(BaseModel):
+    link_id: str
+    user_id: str
 def has_already_voted(user_id: str, link_id: str) -> bool:
     result = supabase.table("votes")\
         .select("user_id")\
